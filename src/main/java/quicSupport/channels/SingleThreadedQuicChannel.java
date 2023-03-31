74e072978e8bac82d1aaa16406c9754c7b401ea8
Daniel João <d.joao@campus.fct.unl.pt>
2023-03-31 12:40:15 +0100

    Added override keyword to SingleThreadedQuicChannel class

--- src/main/java/quicSupport/channels/SingleThreadedQuicChannel.java
+++ src/main/java/quicSupport/channels/SingleThreadedQuicChannel.java
@@ -2,8 +2,6 @@ package quicSupport.channels;
 
 import io.netty.incubator.codec.quic.QuicStreamChannel;
 import io.netty.util.concurrent.DefaultEventExecutor;
-import org.streamingAPI.utils.MetricsDisabledException;
-import quicSupport.handlers.channelFuncHandlers.OldMetricsHandler;
 import quicSupport.handlers.channelFuncHandlers.QuicConnectionMetricsHandler;
 import quicSupport.handlers.channelFuncHandlers.QuicReadMetricsHandler;
 
@@ -21,21 +19,24 @@ public abstract class SingleThreadedQuicChannel extends CustomQuicChannel {
 
     /*********************************** Stream Handlers **********************************/
 
+    @Override
     public void streamErrorHandler(QuicStreamChannel channel, Throwable throwable) {
         executor.execute(() -> super.streamErrorHandler(channel,throwable));
     }
 
+    @Override
     public void streamClosedHandler(QuicStreamChannel channel) {
         executor.execute(() -> super.streamClosedHandler(channel));
     }
-
+    @Override
     public void streamCreatedHandler(QuicStreamChannel channel) {
         executor.execute(() -> super.streamCreatedHandler(channel));
     }
-
+    @Override
     public void streamReader(String streamId, byte[] bytes){
         executor.execute(() -> super.streamReader(streamId,bytes));
     }
+    @Override
     public void onKeepAliveMessage(String parentId){
         executor.execute(() -> super.onKeepAliveMessage(parentId));
     }
@@ -43,9 +44,11 @@ public abstract class SingleThreadedQuicChannel extends CustomQuicChannel {
     /********************************** Stream Handlers **********************************/
 
     /*********************************** Channel Handlers **********************************/
+    @Override
     public void channelActive(QuicStreamChannel streamChannel, byte [] controlData,InetSocketAddress remotePeer){
         executor.execute(() -> super.channelActive(streamChannel,controlData,remotePeer));
     }
+    @Override
     public  void channelInactive(String channelId){
         executor.execute(() -> super.channelInactive(channelId));
     }
@@ -53,49 +56,58 @@ public abstract class SingleThreadedQuicChannel extends CustomQuicChannel {
     /*********************************** Channel Handlers **********************************/
 
     /*********************************** User Actions **************************************/
-
+    @Override
     public void openConnection(InetSocketAddress peer) {
         executor.execute(() -> super.openConnection(peer));
     }
+    @Override
     public void closeConnection(InetSocketAddress peer){
         executor.execute(() -> super.closeConnection(peer));
     }
+    @Override
     public void getStats(InetSocketAddress peer, QuicConnectionMetricsHandler handler){
         executor.execute(() -> super.getStats(peer,handler));
     }
+    @Override
     public void createStream(InetSocketAddress peer) {
         executor.execute(() -> super.createStream(peer));
     }
+    @Override
     public void closeStream(String streamId){
         executor.execute(() -> super.closeStream(streamId));
     }
+    @Override
     public void readMetrics(QuicReadMetricsHandler handler){
         executor.execute(() -> super.readMetrics(handler));
     }
+    @Override
     public void send(String streamId,byte[] message, int len) {
         executor.execute(() -> super.send(streamId,message,len));
     }
+    @Override
     public void send(InetSocketAddress peer,byte[] message, int len) {
         executor.execute(() -> send(peer,message,len));
     }
     /*********************************** User Actions **************************************/
 
     /*********************************** Other Actions *************************************/
+    @Override
     protected void onOutboundConnectionUp() {
         executor.execute(super::onOutboundConnectionDown);
     }
+    @Override
     protected void onOutboundConnectionDown() {
         executor.execute(super::onOutboundConnectionDown);
     }
-
+    @Override
     protected void onInboundConnectionUp() {
         executor.execute(super::onInboundConnectionUp);
     }
-
+    @Override
     protected void onInboundConnectionDown() {
         executor.execute(super::onInboundConnectionDown);
     }
-
+    @Override
     public void onServerSocketClose(boolean success, Throwable cause) {
         executor.execute(() -> onServerSocketClose(success,cause));
     }
