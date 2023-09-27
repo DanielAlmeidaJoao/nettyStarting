dcaf80712797a9526cfb0cd3e676bfba8ebaa97a
Daniel de Almeida <d.joao@campus.fct.unl.pt>
2023-09-27 21:05:16 +0100

    Added more methods to BabelOutputStream

--- src/main/java/tcpSupport/tcpChannelAPI/utils/BabelOutputStream.java
+++ src/main/java/tcpSupport/tcpChannelAPI/utils/BabelOutputStream.java
@@ -2,6 +2,11 @@ package tcpSupport.tcpChannelAPI.utils;
 
 import io.netty.buffer.ByteBuf;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class BabelOutputStream {
 
     private final ByteBuf buf;
@@ -45,6 +50,7 @@ public class BabelOutputStream {
     }
     public int readBytes(byte[] dst, int dstIndex, int length){
         int available = buf.readableBytes();
+
         if(length>available){
             length = available;
         }
@@ -54,6 +60,19 @@ public class BabelOutputStream {
             release();
         }
         return length;
+    }
+
+    public void readBytes(OutputStream outputStream, int available) throws IOException {
+        buf.readBytes(outputStream, available);
+    }
+    public void readBytes(OutputStream outputStream) throws IOException {
+        buf.readBytes(outputStream,buf.readableBytes());
+    }
+    public void readBytes(ByteBuffer outputStream) throws IOException {
+        buf.readBytes(outputStream);
+    }
+    public void readBytes(FileChannel out, long position, int length) throws IOException {
+        buf.readBytes(out,position,length);
     }
     public void release(){
         buf.release();
