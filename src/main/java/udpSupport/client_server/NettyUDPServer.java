2c27864c9cec5881b133aedad7c9612a70c1ac99
Daniel de Almeida <d.joao@campus.fct.unl.pt>
2023-10-02 23:50:58 +0100

    REFACTORING 4. RESET TO UDP USING GROOP THREADS TO SCHEDULE RETRANS

--- src/main/java/udpSupport/client_server/NettyUDPServer.java
+++ src/main/java/udpSupport/client_server/NettyUDPServer.java
@@ -109,7 +109,7 @@ public class NettyUDPServer {
             return;
         }
         UDPWaitForAckWrapper udpWaitForAckWrapper = waitingForAcks.get(msgId);
-        ScheduledFuture scheduledFuture = channel.eventLoop().schedule(() -> {
+        ScheduledFuture scheduledFuture = group.next().schedule(() -> {
             if(!waitingForAcks.containsKey(msgId)) {
                 releaseBuffer(packet);
                 return;
